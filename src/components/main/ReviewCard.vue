b4edb37002605920139cb7698d9f9156c128d3d2
TheKaiser8 <stefano.vanzini8@gmail.com>
2022-12-07 22:22:39 +0100

    Completa ReviewsSection Aggiunge reviews-section-footer

--- src/components/main/ReviewCard.vue
+++ src/components/main/ReviewCard.vue
@@ -11,35 +11,58 @@ export default {
   <div class="review-card">
     <h4 class="review-card__title">{{ infoReview.title }}</h4>
     <p class="review-card__text">{{ infoReview.text }}</p>
-
-    <img
-      class="review-card__avatar"
-      :src="`src/assets/img/${infoReview.reviewer.avatar}`"
-      :alt="infoReview.reviewer.name"
-    />
-    <h5>{{ infoReview.reviewer.name }}</h5>
-    <span>{{ infoReview.reviewer.role }}</span>
+    <div class="review-card__reviewer">
+      <img
+        class="review-card__avatar"
+        :src="`src/assets/img/${infoReview.reviewer.avatar}`"
+        :alt="infoReview.reviewer.name"
+      />
+      <div class="review-card__info">
+        <h5 class="review-card__name">
+          {{ infoReview.reviewer.name }}
+        </h5>
+        <small>{{ infoReview.reviewer.role }}</small>
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .review-card {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
   padding: 3rem;
   background-color: var(--third-bg-color);
   margin: 0 1.5rem;
   border-radius: 0.3125rem;
-  .review-card__avatar {
-    height: 4.375rem;
-    width: 4.375rem;
-    border-radius: 50%;
-    object-fit: cover;
-  }
   .review-card__title {
     color: var(--second-text-color);
     margin-bottom: 1.5rem;
+    font-size: 1.2rem;
+    font-weight: 600;
   }
   .review-card__text {
     margin-bottom: 2.5rem;
+  }
+  .review-card__reviewer {
+    display: flex;
+    align-items: center;
+    .review-card__avatar {
+      height: 4.375rem;
+      width: 4.375rem;
+      border-radius: 50%;
+      object-fit: cover;
+    }
+    .review-card__info {
+      margin-left: 1.25rem;
+      .review-card__name {
+        color: var(--second-text-color);
+        font-size: 1.1rem;
+        font-weight: 600;
+      }
+    }
   }
 }
 </style>
--- src/components/main/ReviewsSection.vue
+++ src/components/main/ReviewsSection.vue
@@ -10,21 +10,21 @@ export default {
     return {
       reviews: [
         {
-          title: "High level of efficiency and scientific teaching methods",
-          text: "I am free to learn at my own pace, follow my own schedule and choose the subject I want to learn from the syllabus. Great study portal for people like me.",
-          reviewer: {
-            avatar: "testimonial-avata-02.jpg",
-            name: "MINA HOLLACE",
-            role: "/ Freelancer",
-          },
-        },
-        {
           title: "It's a choice of quality for people with special needs",
           text: "I'm a very strict person so I require everything to be organized and neat. Then, I'll be able to make things right and shine. MaxCoach guys just got me.",
           reviewer: {
             avatar: "testimonial-avata-03.jpg",
             name: "FLORENCE THEMES",
             role: "/ Multimedia Admin",
+          },
+        },
+        {
+          title: "High level of efficiency and scientific teaching methods",
+          text: "I am free to learn at my own pace, follow my own schedule and choose the subject I want to learn from the syllabus. Great study portal for people like me.",
+          reviewer: {
+            avatar: "testimonial-avata-02.jpg",
+            name: "MINA HOLLACE",
+            role: "/ Freelancer",
           },
         },
         {
@@ -56,10 +56,28 @@ export default {
         <ReviewCard :infoReview="review" />
       </div>
     </div>
+    <div class="reviews-section-footer">
+      <h3 class="reviews-section-footer__subtitle">
+        Start today for getting
+        <span class="text-green">Online Certification</span>
+      </h3>
+      <h2 class="reviews-section-footer__title">
+        You can be your own guiding star with our help!
+      </h2>
+      <button class="btn-green">Get started now</button>
+      <div class="bg-img incomplete-circles">
+        <img src="../../assets/img/maxcoach-shape-02.png" />
+      </div>
+      <div class="bg-img three-lines">
+        <img src="../../assets/img/maxcoach-shape-01.png" />
+      </div>
+    </div>
   </section>
 </template>
 
 <style lang="scss" scoped>
+@use "../../style/partials/mixins.scss" as *;
+
 #reviews-section {
   background-color: var(--second-bg-color);
   padding: 6.25rem 1.875rem;
@@ -70,18 +88,54 @@ export default {
       font-size: 1.7rem;
       color: var(--second-text-color);
       margin-top: 0.5rem;
-
-      .text-green {
-        color: var(--primary-color);
-        font-weight: 400;
-      }
     }
+  }
+  .text-green {
+    color: var(--primary-color);
+    font-weight: 400;
   }
   .row {
     display: flex;
     margin-top: 3rem;
     .col {
       width: calc(100% / 3);
+    }
+  }
+  .reviews-section-footer {
+    color: var(--second-text-color);
+    text-align: center;
+    padding-top: 5.625rem;
+    position: relative;
+
+    &::before {
+      content: "";
+      display: block;
+      position: absolute;
+      left: 14%;
+      width: 3.125rem;
+      height: 3.125rem;
+      border-radius: 50%;
+      border: 0.4375rem solid #e6dcd2;
+    }
+    .reviews-section-footer__subtitle {
+      font-weight: 600;
+    }
+    .reviews-section-footer__title {
+      margin-bottom: 2rem;
+    }
+    .btn-green {
+      @include btn-bg-color(var(--primary-color), #fff);
+    }
+    .bg-img {
+      position: absolute;
+      &.incomplete-circles {
+        bottom: -15%;
+        right: 11%;
+      }
+      &.three-lines {
+        left: 15%;
+        bottom: 15%;
+      }
     }
   }
 }
